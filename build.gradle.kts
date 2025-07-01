0e4be0db0af8fe94bef4317bc1f94dceeac38bd1
Carsten Heinrich <carshei@web.de>
2025-07-02 01:04:45 +0200

    Use gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,6 @@
-import java.time.Duration
-
 plugins {
     `java-library`
     alias(libs.plugins.blossom)
-
-    `maven-publish`
-    signing
-    alias(libs.plugins.nexuspublish)
 }
 
 // Read env vars (used for publishing generally)
@@ -71,7 +65,7 @@ sourceSets {
                 val gitCommit = System.getenv("GITHUB_SHA")
                 val gitBranch = System.getenv("GITHUB_REF")
                 val group = project.group as String?
-                val artifact = project.name as String?
+                val artifact = project.name
                 property("COMMIT", gitCommit ?: "LOCAL")
                 property("BRANCH", gitBranch ?: "LOCAL")
                 property("GROUP", group ?: "UNKNOWN")
@@ -96,7 +90,7 @@ dependencies {
 
     // Testing
     testImplementation(libs.bundles.junit)
-    testImplementation(project(":testing"))
+    testImplementation(project(":minestom:testing"))
 }
 
 tasks {
@@ -116,98 +110,6 @@ tasks {
             links("https://docs.oracle.com/en/java/javase/${javaVersion}/docs/api/")
             links("https://javadoc.io/doc/net.kyori/adventure-api/${libs.versions.adventure.get()}/")
         }
-    }
-
-    nexusPublishing {
-        useStaging.set(true)
-        this.packageGroup.set("net.minestom")
-
-        transitionCheckOptions {
-            maxRetries.set(360) // 1 hour
-            delayBetween.set(Duration.ofSeconds(10))
-        }
-
-        repositories.sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-
-            if (System.getenv("SONATYPE_USERNAME") != null) {
-                username.set(System.getenv("SONATYPE_USERNAME"))
-                password.set(System.getenv("SONATYPE_PASSWORD"))
-            }
-        }
-    }
-
-    publishing.publications.create<MavenPublication>("maven") {
-        groupId = "net.minestom"
-        // todo: decide on publishing scheme
-        artifactId = "minestom-skyblock"
-        version = project.version.toString()
-
-        from(project.components["java"])
-
-        pom {
-            name.set(this@create.artifactId)
-            description.set(shortDescription)
-            url.set("https://github.com/minestom/minestom")
-
-            licenses {
-                license {
-                    name.set("Apache 2.0")
-                    url.set("https://github.com/minestom/minestom/blob/main/LICENSE")
-                }
-            }
-
-            developers {
-                developer {
-                    id.set("TheMode")
-                }
-                developer {
-                    id.set("mworzala")
-                    name.set("Matt Worzala")
-                    email.set("matt@hollowcube.dev")
-                }
-            }
-
-            issueManagement {
-                system.set("GitHub")
-                url.set("https://github.com/minestom/minestom/issues")
-            }
-
-            scm {
-                connection.set("scm:git:git://github.com/minestom/minestom.git")
-                developerConnection.set("scm:git:git@github.com:minestom/minestom.git")
-                url.set("https://github.com/minestom/minestom")
-                tag.set("HEAD")
-            }
-
-            ciManagement {
-                system.set("Github Actions")
-                url.set("https://github.com/minestom/minestom/actions")
-            }
-        }
-    }
-
-    signing {
-        isRequired = System.getenv("CI") != null
-
-        val privateKey = System.getenv("GPG_PRIVATE_KEY")
-        val keyPassphrase = System.getenv()["GPG_PASSPHRASE"]
-        useInMemoryPgpKeys(privateKey, keyPassphrase)
-
-        sign(publishing.publications)
-    }
-}
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") { // Use create<Type>("name") for publication definition
-            from(components.getByName("java")) // Use getByName for components
-            // You can customize artifactId if needed, but it should match your project's artifactId
-            // artifactId = "minestom-snapshots" // This should be picked up automatically
-        }
-    }
-    repositories {
-        mavenLocal() // This publishes to your local .m2 repository
     }
 }
 
